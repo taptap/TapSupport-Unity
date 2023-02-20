d0c1bfd0d366782ed65b31cf7fa2adc0f45b8a6d
ci-gitlab <tds-developer@xd.com>
2023-02-20 16:01:19 +0800

    feat:update upm

---
+++ Runtime/Public/TapSupportDelegate.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TapTap.Support {
+    public class TapSupportDelegate {
+        public Action<bool> OnUnreadStatusChanged { get; set; }
+        public Action<Exception> OnGetUnreadStatusError { get; set; }
+    }
+}
