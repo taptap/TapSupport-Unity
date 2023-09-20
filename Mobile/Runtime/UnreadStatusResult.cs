2c19c4cb76df7357356e714e426c6ac5a2602eda
onerain <onerain88@gmail.com>
2023-09-20 15:15:02 +0800

    feat:update upm

---
+++ Mobile/Runtime/UnreadStatusResult.cs
@@ -0,0 +1,18 @@
+﻿using LC.Newtonsoft.Json;
+
+namespace TapTap.Support.Internal.Platform {
+    public class UnreadStatusResult {
+        [JsonProperty("code")]
+        public int Code { get; private set; }
+
+        [JsonProperty("errorMsg")]
+        public string ErrorMessage { get; private set; }
+
+        [JsonProperty("hasUnRead")]
+        private int hasUnraed { get; set; }
+
+        public bool HasUnread => hasUnraed == 1;
+
+        public bool IsSuccessful => Code == 0;
+    }
+}
