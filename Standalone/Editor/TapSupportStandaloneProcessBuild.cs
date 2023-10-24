0457fa2aa7b09997c8b434ae0038b7ded6d99fcd
onerain <onerain88@gmail.com>
2023-10-24 11:53:56 +0800

    feat:update upm

---
+++ Standalone/Editor/TapSupportStandaloneProcessBuild.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEditor.Build.Reporting;
+using TapTap.Common.Editor;
+
+namespace TapTap.Support.Editor {
+    public class TapSupportStandaloneProcessBuild : SDKLinkProcessBuild {
+        public override int callbackOrder => 0;
+
+        public override string LinkPath => "TapTap/Support/link.xml";
+
+        public override LinkedAssembly[] LinkedAssemblies => new LinkedAssembly[] {
+                    new LinkedAssembly { Fullname = "TapTap.Support.Runtime" },
+                    new LinkedAssembly { Fullname = "TapTap.Support.Standalone.Runtime" }
+                };
+
+        public override Func<BuildReport, bool> IsTargetPlatform => (report) => {
+            return BuildTargetUtils.IsSupportStandalone(report.summary.platform);
+        };
+    }
+}
+
