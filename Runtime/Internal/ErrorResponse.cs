d84c4a203ca79768b546040aa61ee74f8472987b
ci-gitlab <tds-developer@xd.com>
2024-01-29 16:13:50 +0800

    feat:update upm

---
+++ Runtime/Internal/ErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LC.Newtonsoft.Json;
+
+namespace TapTap.Support.Internal {
+    public class ErrorResponse {
+        [JsonProperty("numCode")]
+        public int Code { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
