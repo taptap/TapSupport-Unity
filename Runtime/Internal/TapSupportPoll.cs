c59988d48a78f7124febdbd521c016ff08291715
ci-gitlab <tds-developer@xd.com>
2024-03-04 20:11:58 +0800

    feat:update upm

---
+++ Runtime/Internal/TapSupportPoll.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TapTap.Support.Internal {
+    internal class TapSupportPoll : MonoBehaviour {
+
+    }
+}
