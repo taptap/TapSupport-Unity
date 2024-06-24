354c375a6e3da0c47c7f352f425a4c09a1cc5ec5
ci-gitlab <tds-developer@xd.com>
2024-06-24 18:22:31 +0800

    feat:update upm

---
+++ Mobile/Runtime/SupportUrlResult.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace TapTap.Support.Internal.Platform {
+    public class SupportUrlResult {
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
+
---
+++ Standalone/Runtime/TapSupportStandalone.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_STANDALONE || UNITY_EDITOR
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Newtonsoft.Json;
+using TapTap.Common;
+
+[assembly: UnityEngine.Scripting.Preserve]
+namespace TapTap.Support.Internal.Platform {
+    public class TapSupportStandalone : ITapSupport {
+        public void Init(string serverUrl, string productID, TapSupportDelegate supportDelegate)
+        {
+        }
+
+        public void OpenSupportView(string path){
+            Application.OpenURL(path);
+        }
+
+        public void CloseSupportView() {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
+
+#endif
