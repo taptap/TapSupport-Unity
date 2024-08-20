ee284a820133546b2a3859ffaebaf944a421372b
ci-gitlab <tds-developer@xd.com>
2024-08-20 12:09:45 +0800

    feat:update upm

---
+++ Runtime/Internal/ITapSupport.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TapTap.Support.Internal.Platform {
+    public interface ITapSupport
+    {
+        void Init(string serverUrl, string productID, TapSupportDelegate supportDelegate);
+        void OpenSupportView(string path);
+        void CloseSupportView();
+    }
+}
